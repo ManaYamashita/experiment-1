f3c8c6b8a9eb866feb15406e17377d429a418263
ManaYamashita <110384795+ManaYamashita@users.noreply.github.com>
2022-08-26 18:35:44 +0900

    Add files via upload

--- SPT_ex1_try.js
+++ SPT_ex1_try.js
@@ -3,14 +3,6 @@ var repo_site = "https://manayamashita.github.io/SPT_ex1_try/";
 /*実験はじめ*/
 var timeline = [];
 
-jsPsych.init({
-      timeline: timeline,
-      on_finish: function() {
-        jsPsych.data.displayData();
-        }
-    });
-
-
 var welcome = {
         type : "html-keyboard-response",
         stimulus : "何かキーを押すと実験が始まります",
