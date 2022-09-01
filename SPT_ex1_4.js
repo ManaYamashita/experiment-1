326b4a7954d70aa1222cc145808d9efa81f6f4e6
ManaYamashita <110384795+ManaYamashita@users.noreply.github.com>
2022-09-01 21:03:04 +0900

    Add files via upload

--- SPT_ex1_4.js
+++ SPT_ex1_4.js
@@ -28,15 +28,14 @@ var preload = {
           ]
 }
 /*画像読み込み終わり*/
-var swid = document.documentElement.clientWidth*0.1;
-var shgt = document.documentElement.clientHeight*0.1;
+var swid = document.documentElement.clientWidth*0.8;
  
 /*実験はじめ*/
 
 
 var welcome = {
         type : "html-keyboard-response",
-        stimulus : "何かキーを押すと実験が始まりますaaaaaaaaaaaa",
+        stimulus : "何かキーを押すと実験が始まります",
         };
  
         timeline.push(welcome);
