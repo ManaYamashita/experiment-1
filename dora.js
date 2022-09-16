26e350163b1188bac42417a72a127e265ee6bfb1
ManaYamashita <110384795+ManaYamashita@users.noreply.github.com>
2022-09-16 14:05:13 +0900

    Add files via upload

--- dora.js
+++ dora.js
@@ -137,7 +137,7 @@ var test_rand = jsPsych.randomization.sampleWithReplacement(test_stimuli, 4);
 var test = {
       type:"image-keyboard-response",
        stimulus: jsPsych.timelineVariable('cue'),
-       stimulus_width: document.documentElement.clientWidth*0.4,
+       stimulus_width: document.documentElement.clientWidth*0.6,
        choices: 'NO_KEYS',
        trial_duration: 1000,
         };
@@ -154,7 +154,7 @@ var br = {
  var test1 = {
   type: 'image-button-response',
   stimulus: jsPsych.timelineVariable('target'),
-  stimulus_width: document.documentElement.clientWidth*0.4,
+  stimulus_width: document.documentElement.clientWidth*0.6,
   choices: ['同じ','違う'],
   data: {
     task: 'response',
