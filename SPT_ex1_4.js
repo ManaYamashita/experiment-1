15375d77f619ced31d6a03228fd44939d4111ad8
ManaYamashita <110384795+ManaYamashita@users.noreply.github.com>
2022-09-21 16:04:58 +0900

    Add files via upload

--- SPT_ex1_4.js
+++ SPT_ex1_4.js
@@ -32,63 +32,62 @@ timeline.push(preload);
 var swid = document.documentElement.clientWidth;
  
 /*実験はじめ*/
-
-
 var welcome = {
-        type : "html-keyboard-response",
-        stimulus : "何かキーを押すと実験が始まります",
-        };
- 
-        timeline.push(welcome);
+  type : "html-keyboard-response",
+  stimulus : "何かキーを押すと実験が始まります1",
+  };
 
- /*教示はじめ*/
+  timeline.push(welcome);
+
+/*教示はじめ*/
 var instructions = {
-      type: "html-keyboard-response",
-      stimulus: "<p>教示</p>" 
-                 };
+type: "html-keyboard-response",
+stimulus: "<p>教示</p>" 
+           };
 
-        timeline.push(instructions);
+  timeline.push(instructions);
+
     
 /*教示おわり*/
 
 var test_stimuli = [
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf0.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf40.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf80.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf120.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf160.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rkf40.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rkf80.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rkf120.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rkf160.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk0.png',  correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk40.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk80.png',  correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk120.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk160.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rfk40.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rfk80.png',  correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rfk120.png', correct_response: 'f'},
-  { cue:repo_site + 'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rfk160.png', correct_response: 'f'},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf0.png', correct_response: 'j', kaku: kf0},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf40.png', correct_response: 'j', kaku: kf40},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf80.png', correct_response: 'j', kaku: kf80},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf120.png', correct_response: 'j', kaku: kf120},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/kf160.png', correct_response: 'j', kaku: kf160},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rkf40.png', correct_response: 'j', kaku: Rkf40},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rkf80.png', correct_response: 'j', kaku: Rkf80},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rkf120.png', correct_response: 'j', kaku: Rkf120},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rkf160.png', correct_response: 'j', kaku: Rkf160},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk0.png',  correct_response: 'f', kaku: fk0},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk40.png', correct_response: 'f', kaku: fk40},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk80.png',  correct_response: 'f', kaku: fk80},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk120.png', correct_response: 'f', kaku: fk120},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/fk160.png', correct_response: 'f', kaku: fk160},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rfk40.png', correct_response: 'f', kaku: Rfk40},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rfk80.png',  correct_response: 'f', kaku: Rfk80},
+  { cue: repo_site +'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rfk120.png', correct_response: 'f', kaku: Rfk120},
+  { cue:repo_site + 'SPT_exp1_image/flower.png', target:  repo_site +'SPT_exp1_image/Rfk160.png', correct_response: 'f', kaku: Rfk160},
   
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf0.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf40.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf80.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf120.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf160.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rkf40.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rkf80.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rkf120.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rkf160.png', correct_response: 'f'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk0.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk40.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk80.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk120.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk160.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rfk40.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rfk80.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rfk120.png', correct_response: 'j'},
-  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rfk160.png', correct_response: 'j'},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf0.png', correct_response: 'f', kaku: kf0},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf40.png', correct_response: 'f', kaku: kf40},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf80.png', correct_response: 'f', kaku: kf80},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf120.png', correct_response: 'f', kaku: kf120},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/kf160.png', correct_response: 'f', kaku: kf160},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rkf40.png', correct_response: 'f', kaku: Rkf40},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rkf80.png', correct_response: 'f', kaku: Rkf80},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rkf120.png', correct_response: 'f', kaku: Rkf120},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rkf160.png', correct_response: 'f', kaku: kf160},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk0.png', correct_response: 'j', kaku: fk0},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk40.png', correct_response: 'j', kaku: fk40},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk80.png', correct_response: 'j', kaku: fk80},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk120.png', correct_response: 'j', kaku: fk120},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/fk160.png', correct_response: 'j', kaku: fk160},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rfk40.png', correct_response: 'j', kaku: Rfk40},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rfk80.png', correct_response: 'j', kaku: Rfk80},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rfk120.png', correct_response: 'j', kaku: Rfk120},
+  { cue: repo_site +'SPT_exp1_image/ken.png', target: repo_site +'SPT_exp1_image/Rfk160.png', correct_response: 'j', kaku: Rfk160},
 
 ];
 
@@ -96,10 +95,11 @@ var test_stimuli = [
     
 
 /*試行開始*/
+
 var ready = {
-      type: "html-keyboard-response",
-      stimulus: "<p>Ready？</p>" 
-                    };
+  type: "html-keyboard-response",
+  stimulus: "<p>Ready？</p>" 
+                };
 
  var br = {
       type: "html-keyboard-response",
@@ -138,18 +138,12 @@ var br = {
   
   on_finish: function(data){
     data.correct = jsPsych.pluginAPI.compareKeys(data.response, data.correct_response);
+    data.flowerken = jsPsych.timelineVariable('cue');
+    data.target = jsPsych.timelineVariable('target'); 
+    data.kaku = jsPsych.timelineVariable('kaku');
   }
 };
-         
-
-/*記録*/         
-         on_finish: (data) => {
-Qualtrics.SurveyEngine.setEmbeddeedData("datajs", datajs);    
-
-data.flowerken = jsPsych.timelineVariable('cue');
-
-data.target = jsPsych.timelineVariable('target');
-};
+        
 
 
   var purosdure={
