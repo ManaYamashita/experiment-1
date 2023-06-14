6879f3e6064850f9a5749ffc452f98f03b88ba79
ManaYamashita <110384795+ManaYamashita@users.noreply.github.com>
2023-06-14 17:53:09 +0900

    Add files via upload

--- M1-1.js
+++ M1-1.js
@@ -160,7 +160,7 @@ var welcome = {
     timeline:[ready, fy, br, face, table, test],
     timeline_variables: test_stimuli,
     randomize_order: true,
-    repetitions: 1,
+    repetitions: 2,
 }
 
 timeline.push(purosdure);
