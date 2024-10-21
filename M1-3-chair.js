db041e6e9f266e52dc0660b7564a01d97ce8c827
ManaYamashita <110384795+ManaYamashita@users.noreply.github.com>
2024-10-21 15:41:56 +0900

    Add files via upload

---
+++ M1-3-chair.js
@@ -0,0 +1,105 @@
+var repo_site = "https://manayamashita.github.io/experiment-1/";
+
+
+var timeline = [];
+
+var preload = {
+  type: 'preload',
+  images: [
+  repo_site + "3human/koshiten.png" ,
+
+  repo_site + '3human/left45.png',
+  repo_site + '3human/left90.png',
+  repo_site + '3human/left135.png',
+  repo_site + '3human/left225.png',
+  repo_site + '3human/left270.png',
+  repo_site + '3human/left315.png',
+
+  repo_site + '3human/right45.png',
+  repo_site + '3human/right90.png',
+  repo_site + '3human/right135.png',
+  repo_site + '3human/right225.png',
+  repo_site + '3human/right270.png',
+  repo_site + '3human/right315.png',
+   ]
+    }
+    timeline.push(preload);
+
+/*画像読み込み*/
+
+/*画像読み込み終わり*/
+
+var swid = document.documentElement.clientWidth;
+
+/*実験はじめ*/
+
+/*教示*/
+var welcome = {
+  type : "html-keyboard-response",
+  stimulus : "何かキーを押すと実験が始まります",
+  };
+
+  timeline.push(welcome);
+
+  var test_stimuli = [
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/left45.png',  correct_response: 'f', stimu: 'left45'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/left90.png',  correct_response: 'f', stimu: 'left90'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/left135.png',  correct_response: 'f', stimu: 'left135'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/left225.png',  correct_response: 'f', stimu: 'left225'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/left270.png',  correct_response: 'f', stimu: 'left270'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/left315.png',  correct_response: 'f', stimu: 'left315'},
+
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/right45.png',  correct_response: 'j', stimu: 'right45'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/right90.png',  correct_response: 'j', stimu: 'right90'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/right135.png',  correct_response: 'j', stimu: 'right135'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/right225.png',  correct_response: 'j', stimu: 'right225'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/right270.png',  correct_response: 'j', stimu: 'right270'},
+    {koshiten:repo_site + "3human/koshiten.png" , target : repo_site + '3human/right315.png',  correct_response: 'j', stimu: 'right315'},
+  ]
+
+
+  var ready = {
+    type: "html-keyboard-response",
+    stimulus: "<p>何かキーを押すと実験が始まります</p>" 
+                  };
+  
+                  
+ var fy = {
+      type: "image-keyboard-response",
+      stimulus: jsPsych.timelineVariable('koshiten'), 
+      choice: 'NO_KEYS',
+      trial_duration: function(){
+        return jsPsych.randomization.sampleWithoutReplacement([Math.floor( Math.random() * 1000 ) + 1500], 1)[0];//Math.random() * ( 最大値 - 最小値 ) + 最小値
+      },
+                    };
+
+
+  var test = {
+  type:"image-keyboard-response",
+  stimulus: jsPsych.timelineVariable('target'),
+  choices: ['f', 'j'],
+  //stimulus_width: document.documentElement.clientWidth*0.6,
+  data: {
+    task: 'response',
+    correct_response: jsPsych.timelineVariable('correct_response')
+  },
+  
+  on_finish: function(data){
+    data.correct = jsPsych.pluginAPI.compareKeys(data.response, data.correct_response); 
+    data.stimu = jsPsych.timelineVariable('stimu');
+  }
+};
+        
+
+
+  var purosdure={
+    timeline:[fy, table, test],
+    timeline_variables: test_stimuli,
+    randomize_order: true,
+    repetitions: 6,
+}
+
+timeline.push(purosdure);
+
+
+
