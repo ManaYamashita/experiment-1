586d47787480799b37f9068fb0b16a3c10d9f1cd
ManaYamashita <110384795+ManaYamashita@users.noreply.github.com>
2024-10-21 19:13:12 +0900

    Add files via upload

--- M1-3try.js
+++ M1-3try.js
@@ -74,7 +74,7 @@ var welcome = {
 
   var br = {
      type: "image-keyboard-response",
-      stimulus:"3try/koshiten1.png", 
+      stimulus:'3try/koshiten1.png', 
       choice: 'NO_KEYS',
       trial_duration: function(){
         return jsPsych.randomization.sampleWithoutReplacement([Math.floor( Math.random() * 1000 ) + 1500], 1)[0];//Math.random() * ( 最大値 - 最小値 ) + 最小値
